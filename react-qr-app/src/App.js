3949babbbe7e3e05d327090d98d740c3bdddaf22
satyasuranjeet <satyajena911@gmail.com>
2024-11-08 17:59:09 +0530

    Added the file"

--- react-qr-app/src/App.js
+++ react-qr-app/src/App.js
@@ -4,7 +4,7 @@ import Login from "./Login";
 import QRGenerator from "./QRGenerator";
 import "./App.css";
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = ucdseState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const storedHash ="0d45f5fd462b8c70bffb10021ac1bcff3f58f29b1faf7568595095427d42812c";
   const handleLogin = (password) => {
     const hashedPassword = CryptoJS.SHA256(password).toString();
